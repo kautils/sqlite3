a905d721e7a4af48cf4240ffd703544c1981efa6
ka <todo1@rotarydef.net>
2023-09-24 11:37:09 -0700

    install

--- sqlite3/sqlite3.cmake
+++ sqlite3/sqlite3.cmake
@@ -51,7 +51,7 @@ set(${module_name}_common_pref
 )
 
 file(GLOB_RECURSE ${m}_install_headers ${CMAKE_CURRENT_LIST_DIR}/*.h )
-install(FILES ${${m}_install_headers} DESTINATION include/kauitl)
+install(FILES ${${m}_install_headers} DESTINATION include/kautil)
 
 CMakeLibraryTemplate(${module_name} EXPORT_LIB_TYPE shared ${${module_name}_common_pref} )
 target_link_directories(${${module_name}_shared} PRIVATE ${KAUTIL_LIBSQLITE3_LIBDIR})
